d51959a966ba02de9e946ee9dfc03cbe563fe370
dapraws <darrelprawira26@gmail.com>
2024-05-25 15:30:56 +0700

    feat: update search bar
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

--- app/src/main/java/com/megalogic/tracky/ui/home/HomeFragment.kt
+++ app/src/main/java/com/megalogic/tracky/ui/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.megalogic.tracky.adapter.AssetListAdapter
+import com.megalogic.tracky.data.Asset
 import com.megalogic.tracky.data.DummyData
 import com.megalogic.tracky.databinding.FragmentHomeBinding
 
@@ -14,6 +16,8 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var assetAdapter: AssetListAdapter
+    private var assetList: List<Asset> = DummyData.itemAsset
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +32,22 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Setup RecyclerView
-        val assetAdapter = AssetListAdapter(requireContext(), DummyData.itemAsset)
+        assetAdapter = AssetListAdapter(requireContext(), assetList)
         binding.rvAsset.layoutManager = LinearLayoutManager(context)
         binding.rvAsset.adapter = assetAdapter
+
+        // Setup SearchView
+        binding.svAsset.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // No action on text submit
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                assetAdapter.filter.filter(newText)
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
