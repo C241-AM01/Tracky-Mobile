ef435d26c53f85c2ccc2a6650e6433307447947d
dapraws <darrelprawira26@gmail.com>
2024-05-25 16:22:02 +0700

    feat: update asset list page
    Co-authored-by: dapraws <darrelprawira26@gmail.com>
    Co-authored-by: altriskaa <altriskaa@gmail.com>"

--- app/src/main/java/com/megalogic/tracky/ui/assetlist/AssetListFragment.kt
+++ app/src/main/java/com/megalogic/tracky/ui/assetlist/AssetListFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.megalogic.tracky.adapter.AssetListAdapter
+import com.megalogic.tracky.data.Asset
+import com.megalogic.tracky.data.DummyData
 import com.megalogic.tracky.databinding.FragmentAssetListBinding
+import com.megalogic.tracky.databinding.FragmentHomeBinding
 
 class AssetListFragment : Fragment() {
 
@@ -15,19 +21,38 @@ class AssetListFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var assetAdapter: AssetListAdapter
+    private var assetList: List<Asset> = DummyData.itemAsset
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(AssetListViewModel::class.java)
-
         _binding = FragmentAssetListBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Setup RecyclerView
+        assetAdapter = AssetListAdapter(requireContext(), assetList)
+        binding.rvAsset.layoutManager = LinearLayoutManager(context)
+        binding.rvAsset.adapter = assetAdapter
+
+        // Setup SearchView
+        binding.svAsset.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // No action on text submit
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                assetAdapter.filter.filter(newText)
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
