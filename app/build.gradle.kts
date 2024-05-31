a388f523d770554834ed3045734096e69ccdb626
altriskaa <114663340+altriskaa@users.noreply.github.com>
2024-05-31 19:55:33 +0700

    feat: update home & asset list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.androidx.activity)
     implementation(libs.play.services.maps)
+    implementation(libs.car.ui.lib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
